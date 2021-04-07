337191350521b6f1bd30d1b3fa9bc85c3b329629
Peter Moser <p.moser@noi.bz.it>
2021-04-07 15:18:04 +0200

    infra: Use new aws ecr login to pull docker images from restricted areas

--- infrastructure/jenkins/test.groovy
+++ infrastructure/jenkins/test.groovy
@@ -1,42 +1,53 @@
 pipeline {
-    agent {
-        docker {
-			image '755952719952.dkr.ecr.eu-west-1.amazonaws.com/webcompbuild:latest'
+	agent any
+	environment {
+		DOCKER_IMAGE = "755952719952.dkr.ecr.eu-west-1.amazonaws.com/webcompbuild:latest"
+	}
+	options {
+		ansiColor('xterm')
+	}
+	stages {
+		stage('Prepare') {
+			steps {
+				sh '''
+					rm -rf $(jq -r ".dist.basePath" wcs-manifest.json)
+					aws ecr get-login --region eu-west-1 --no-include-email | bash
+				'''
+			}
 		}
-    }
-    options {
-        ansiColor('xterm')
-    }
-    stages {
-        stage('Prepare') {
-            steps {
-                sh '''
-					cp /webcompbuild/.env .env
-                    rm -rf $(jq -r ".dist.basePath" wcs-manifest.json)
-                '''
-            }
-        }
-        stage('Dependencies') {
-            steps {
-                sh 'npm ci'
-            }
-        }
-        stage('Build') {
-            steps {
-                sh 'npm run build'
-            }
-        }
+		stage("Build") {
+			agent {
+				dockerfile {
+					filename 'infrastructure/docker/node.dockerfile'
+					additionalBuildArgs '--build-arg JENKINS_USER_ID=$(id -u jenkins) --build-arg JENKINS_GROUP_ID=$(id -g jenkins)'
+				}
+			}
+			steps {
+				sh '''
+					npm ci
+					npm run build
+				'''
+			}
+		}
 		stage('Deploy to Test Store') {
+			agent {
+				docker {
+					alwaysPull true
+					image "${DOCKER_IMAGE}"
+				}
+			}
 			steps {
 				sshagent (credentials: ['tomcatkey', 'jenkins_github_ssh_key']) {
 					sh '''
+						cp /webcompbuild/.env .env
 						WHEN=$(date "+%Y%m%dT%H%M")
 						WC_NAME=${GIT_URL##*/}
 						WC_NAME=${WC_NAME%.git}
+						/webcompbuild/wcstorecli.sh -u
 						/webcompbuild/wcstorecli.sh -d "${WC_NAME}" "${BRANCH_NAME}-${BUILD_NUMBER}-${WHEN}"
 					'''
 				}
 			}
 		}
-    }
+	}
 }
