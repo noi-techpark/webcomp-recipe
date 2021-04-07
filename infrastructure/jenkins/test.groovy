1d577abb51180a95c3d664f1872747fa7da9b05d
Peter Moser <p.moser@noi.bz.it>
2021-04-07 20:05:03 +0200

    infra: new docker agent for test CD

--- infrastructure/jenkins/test.groovy
+++ infrastructure/jenkins/test.groovy
@@ -7,45 +7,60 @@ pipeline {
 		ansiColor('xterm')
 	}
 	stages {
-		stage('Prepare') {
+		stage('AWS ECR login') {
 			steps {
 				sh '''
-					rm -rf $(jq -r ".dist.basePath" wcs-manifest.json)
 					aws ecr get-login --region eu-west-1 --no-include-email | bash
 				'''
 			}
 		}
-		stage("Build") {
-			agent {
-				dockerfile {
-					filename 'infrastructure/docker/node.dockerfile'
-					additionalBuildArgs '--build-arg JENKINS_USER_ID=$(id -u jenkins) --build-arg JENKINS_GROUP_ID=$(id -g jenkins)'
-				}
-			}
-			steps {
-				sh '''
-					npm ci
-					npm run build
-				'''
-			}
-		}
-		stage('Deploy to Test Store') {
+		stage('Agent: Docker webcompbuild') {
 			agent {
 				docker {
 					alwaysPull true
 					image "${DOCKER_IMAGE}"
 				}
 			}
-			steps {
-				sshagent (credentials: ['tomcatkey', 'jenkins_github_ssh_key']) {
-					sh '''
-						cp /webcompbuild/.env .env
-						WHEN=$(date "+%Y%m%dT%H%M")
-						WC_NAME=${GIT_URL##*/}
-						WC_NAME=${WC_NAME%.git}
-						/webcompbuild/wcstorecli.sh -u
-						/webcompbuild/wcstorecli.sh -d "${WC_NAME}" "${BRANCH_NAME}-${BUILD_NUMBER}-${WHEN}"
-					'''
+			stages {
+				stage('Prepare') {
+					steps {
+						sh '''
+							cp /webcompbuild/.env .env
+							rm -rf $(jq -r ".dist.basePath" wcs-manifest.json)
+						'''
+					}
+				}
+				stage("Dependencies") {
+					steps {
+						sh '''
+							npm ci
+						'''
+					}
+				}
+				stage("Build") {
+					steps {
+						sh '''
+							npm run build
+						'''
+					}
+				}
+				stage("Update manifest") {
+					steps {
+						sh "/webcompbuild/wcstorecli.sh -u"
+					}
+				}
+				stage('Deploy') {
+					steps {
+						sshagent (credentials: ['tomcatkey', 'jenkins_github_ssh_key']) {
+							sh '''
+								cp /webcompbuild/.env .env
+								WHEN=$(date "+%Y%m%dT%H%M")
+								WC_NAME=${GIT_URL##*/}
+								WC_NAME=${WC_NAME%.git}
+								/webcompbuild/wcstorecli.sh -d "${WC_NAME}" "${BRANCH_NAME}-${BUILD_NUMBER}-${WHEN}"
+							'''
+						}
+					}
 				}
 			}
 		}
